055090b8bd243834fccf49736c37ed3d30619012
almwld <your-email@example.com>
2026-05-08 20:40:24 +0300

    Fix: notification import after material.dart

--- sehatak_flutter/lib/presentation/screens/home/home_screen.dart
+++ sehatak_flutter/lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
-import 'package:sehatak/presentation/screens/notification/notification_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sehatak/presentation/screens/notification/notification_screen.dart';
 import 'package:sehatak/core/constants/app_colors.dart';
 import 'package:sehatak/core/services/api_service.dart';
 import 'package:sehatak/presentation/widgets/common_widgets.dart';
